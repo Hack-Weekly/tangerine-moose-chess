0a1cf74fa390767e80866af43b7027193fe93c41
Dirty-Bubble <kamath7@purdue.edu>
2023-05-18 03:36:01 -0400

    made chessboard/game with blackpiece AI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,32 +1,61 @@
-import { useState } from "react";
-
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
+import { Chess } from "chess.ts";
+import { Chessboard } from "react-chessboard";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [game, setGame] = useState<Chess>(new Chess());
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
-  );
+  //makes sure black piece moves right after white pieces
+  useEffect(() => {
+    if (game.turn() === "b") {
+      makeRandomMove();
+    }
+  }, [game]);
+
+  //makes sure game state is mutated safely
+  function safeGameMutate(previousGame: (game: Chess) => void): void {
+    setGame((g: Chess) => {
+      const newGame = new Chess(g.fen());
+      previousGame(newGame);
+      return newGame;
+    });
+  }
+
+  //makes random move for black piece AI
+  function makeRandomMove() {
+    const possibleMoves = game.moves();
+    // Exit if the game is over
+    if (game.gameOver()) return;
+
+    // Select a random move
+    const randomIndex = Math.floor(Math.random() * possibleMoves.length);
+    const randomMove = possibleMoves[randomIndex];
+
+    // Play the random move
+    safeGameMutate((game: Chess) => {
+      game.move(randomMove);
+    });
+  }
+
+  //performs move onDrop of chess piece
+  function onDrop(sourceSquare: string, targetSquare: string): boolean {
+    let move = null;
+
+    safeGameMutate((game) => {
+      move = game.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: "q",
+      });
+    });
+
+    if (move === null) return false;
+
+    return true;
+  }
+  //returns the rendered chessboard with the current gamepieces and with the onPieceDrop function
+  return <Chessboard position={game.fen()} onPieceDrop={onDrop} />;
 }
 
 export default App;
